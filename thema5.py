5adbc4c2aca2d976d0949edac01a257bf1b1d743
TheAncientMariner <theancientmariner84@gmail.com>
2016-03-04 16:18:39 +0200

    Θέμα 5
    μπλααααα

---
+++ thema5.py
@@ -0,0 +1,61 @@
+#Kataskeui sinartisis i opoia psaxnei pia imera tis ebdomadas einai i imerominia pou dinei o xristis
+
+def Imerominia(mera,minas,xronia):
+	""" Tin formula me tin opoia tha ipologiso tin imera tis ebdomadas tin vrika sto wikipedia 
+		kai sigkekrimena https://en.wikipedia.org/wiki/Zeller%27s_congruence"""
+
+	q = mera
+	
+	"""minas 3=Martios ... 14=Febrouarios, 
+		auto isxuei gia tin formula,
+		o xristis pliktrologei kanonika, p.x gia ton xristi o febrouarios einai to 2"""
+		
+	if minas == 1:
+		minas = 13
+		xronia = xronia - 1
+	elif minas == 2:
+		minas = 14
+		xronia = xronia - 1
+	
+	#K einai year mod 100
+	K = xronia % 100
+	J = xronia // 100
+	#h einai i formula 
+	h = (q + int(13*(minas + 1)/5.0) + K + int(K/4.0) + int(J/4.0) - 2 * J) % 7
+	
+	if h == 0:
+		h = 7
+		
+	return h	
+	
+
+#Kataskeui kiriou programmatos
+
+mera, minas, xronia = [int(x) for x in raw_input("Pliktrogiste tin imerominia pou thelete, me morfi mera/minas/xronia, gia na sas epistrepsoume ti mera ebdomadas: ").split('/')]
+
+#Klisi sinartisis
+h = Imerominia(mera,minas,xronia)
+
+if h==1:
+	h = "KYRIAKH"
+elif h==2:
+	h = "DEYTERA"
+elif h==3:
+	h = "TRITH"
+elif h ==4:
+	h = "TETARTH" 	
+elif h==5:
+	h = "PEMPTH"
+elif h==6:
+	h = "PARASKEYH"
+else:
+	h = "SABBATO"
+
+if minas==0 or mera==0 or xronia==0:
+	print """Den iparxei minas, mera, i xronia pou na antistoixei sto noumero '0'"""
+
+
+else:
+	print h
+		
+		
